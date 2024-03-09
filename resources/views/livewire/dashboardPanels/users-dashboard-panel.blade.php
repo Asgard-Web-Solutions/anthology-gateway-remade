2607d118e58597caf1c8d3628b912dfc89a1a3cd
Jon Zenor <JonZenor@gmail.com>
2024-03-09 10:19:11 -0700

    Improved the look of the admin mini card

--- resources/views/livewire/dashboardPanels/users-dashboard-panel.blade.php
+++ resources/views/livewire/dashboardPanels/users-dashboard-panel.blade.php
@@ -1,14 +1,12 @@
 <div>
     {{-- If your happiness depends on money, you will never be happy with yourself. --}}
-    <div class="w-48 p-2 m-2 text-black rounded-md bg-slate-300">
-        <h2 class="font-bold text-red-900"><i class="fa-solid fa-users"></i> Users</h2>
-        <p class="m-2 text-sm font-light">
-            New Users: {{ $newUsers }}
-            Total Users: {{ $totalUsers }}
-        </p>
-
-        <div class="text-right">
-            <a  href="{{ route('users') }}" class="px-3 py-1 text-white bg-purple-800 rounded-lg hover:bg-purple-600">Manage</a>
+    <a href="{{ route('users') }}">
+        <div class="w-48 p-2 m-2 text-black rounded-md bg-slate-300 hover:bg-slate-400">
+            <h2 class="font-bold text-red-900"><i class="fa-solid fa-users"></i> Users</h2>
+            <p class="m-2 text-sm font-light">
+                New: <span class="mr-4 font-bold">{{ $newUsers }}</span>
+                Total: <span class="font-bold">{{ $totalUsers }}</span>
+            </p>
         </div>
-    </div>
+    </a>
 </div>
