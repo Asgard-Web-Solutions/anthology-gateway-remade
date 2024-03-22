197cc3006ca6f691db1fccf5b46a72700dc6b6af
Jon Zenor <JonZenor@gmail.com>
2024-03-21 18:02:09 -0600

    Adding to do items

---
+++ tests/Feature/AnthologyTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class AnthologyTest extends TestCase
+{
+    // TODO: Put a "create" button on the dashboard
+
+    // TODO: Put a "create" button on the publisher view page
+
+    // TODO: Anthology create page which gathers limited details
+
+    // TODO: Anthology view page
+
+    // TODO: Anthology view page has different sections
+
+    // TODO: Anthology info edit pages
+
+    // TODO: Upload pics for Anthology header and book cover to an AWS like bucket
+
+    // TODO: Anthologies can change status to "Launch"
+
+    // TODO: Launched anthologies show up on the dashboard if opening for submissions soon
+
+    // TODO: Launched anthologies show up on the home page
+}
