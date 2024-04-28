d4b3367dc09edfd3684d32b014e2eb3601cc651d
Jon Zenor <JonZenor@gmail.com>
2024-04-28 10:18:17 -0600

    Added an author icon

--- config/ag.php
+++ config/ag.php
@@ -4,6 +4,7 @@ return [
     'icons' => [
         'publisher' => env('ICON_PUBLISHER', 'fa-duotone fa-typewriter'),
         'anthology' => env('ICON_ANTHOLOGY', 'fa-duotone fa-book'),
+        'author' => env('ICON_AUTHOR', 'fa-duotone fa-user-astronaut'),
         'edit' => env('ICON_EDIT', 'fa-light fa-pen-to-square'),
         'delete' => env('ICON_DELETE', 'fa-regular fa-trash-can'),
     ],
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -14,7 +14,7 @@
                             @foreach ($user->publishers as $publisher)
                                 <x-content.section heading="{{ $publisher->name }}" heading_icon="{{ config('ag.icons.publisher') }}">
                                     <x-content.paragraph>
-                                        <x-button.primary href="{{ route('publisher.view', $publisher->id) }}" icon="{{ config('ag.icons.publisher') }}">View Publisher Profile</x-button.primary>
+                                        <x-button.dim href="{{ route('publisher.view', $publisher->id) }}" icon="{{ config('ag.icons.publisher') }}">View Publisher Profile</x-button.dim>
                                     </x-content.paragraph>
                                 </x-content.section>
                             @endforeach
@@ -27,15 +27,15 @@
                         @endif
 
                         @if ($user->author)
-                            <x-content.section heading="Author Profile" heading_icon="{{ config('ag.icons.publisher') }}">
+                            <x-content.section heading="Author Profile" heading_icon="{{ config('ag.icons.author') }}">
                                 <x-content.paragraph>
                                     <x-button.dim href="{{ route('author.edit', $user->author->id) }}">Edit Author</x-button.primary>
                                 </x-content.paragraph>
                             </x-content.section>
                         @else
-                            <x-content.section heading="Author Profile" heading_icon="{{ config('ag.icons.publisher') }}">
+                            <x-content.section heading="Author Profile" heading_icon="{{ config('ag.icons.author') }}">
                                 <x-content.paragraph>
-                                    <x-button.dim href="{{ route('author.create') }}">Create Author Profile</x-button.primary>
+                                    <x-button.primary href="{{ route('author.create') }}">Create Author Profile</x-button.primary>
                                 </x-content.paragraph>
                             </x-content.section>
                         @endif
@@ -51,13 +51,15 @@
                 <div class="flex w-full">
 
                     @forelse ($user->anthologies as $anthology)
-                        <x-content.mini-box heading="{{ $anthology->name }}">
+                        <x-content.mini-box heading="{{ $anthology->name }}" href="{{ route('anthology.view', $anthology->id) }}">
                             <div class="items-center h-96">
                                 <a href="{{ route('anthology.view', $anthology->id) }}">
                                     @if ($anthology->cover)
                                         <img src="{{ $anthology->cover }}" class="mx-auto h-96"/>
-                                    @else
+                                    @elseif ($anthology->header)
                                         <img src="{{ $anthology->header }}" class="mx-auto max-h-96"/>
+                                    @else
+                                        No Image Uploaded...
                                     @endif
                                 </a>
                             </div>
@@ -71,7 +73,7 @@
 
                 @can('create', App\Models\Anthology::class)
                     <x-content.button-section>
-                        <x-button.primary-small icon="fa-solid fa-plus" href="{{ route('anthology.create') }}">{{ __('Create Anthology ') }}</x-button.primary-small>
+                        <x-button.primary icon="fa-solid fa-plus" href="{{ route('anthology.create') }}">{{ __('Create Anthology ') }}</x-button.primary>
                     </x-content.button-section>
                 @endcan
             </x-content.box>
